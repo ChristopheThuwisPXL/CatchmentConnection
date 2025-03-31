f90f76a1e5bcf9dbedbbcf8362223f6d99ce7111
ChristopheThuwisPXL <christophe.thuwis@student.pxl.be>
2025-03-31 18:06:29 +0200

    Added the Light/Dark theme and changed some css

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import { ThemeProvider } from "@/components/theme-provider"
 import LoginPage from './pages/Login';
-//import { ModeToggle } from './components/mode-toggle'
+import { ModeToggle } from './components/mode-toggle'
 import './App.css';
 import Dashboard from './pages/dashboard';
 import HistoricData from './pages/historicData'
+import { ThemeProvider } from './components/theme-provider';
 
 function App() {
   return (
-    
+    <ThemeProvider>
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="dashboard" element={<Dashboard/>}/>
-        <Route path="historicData" element={<HistoricData/>}/>
-      </Routes>
+    <div className="app-container">
+    <header className="header">
+            <ModeToggle />
+          </header>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="historicData" element={<HistoricData />} />
+          </Routes>
+        </div>
     </Router>
-    
+    </ThemeProvider>
   );
 }
 
